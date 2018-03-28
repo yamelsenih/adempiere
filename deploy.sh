be6eb7696fc6b65ccf525048f612294ed808d9af
Raul Muñoz <rmunoz@erpcya.com>
2018-03-28 15:42:21 -0430

    Change interpreter

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 scp -o "StrictHostKeyChecking=no" install/build/Adempiere_390LTS.tar.gz $REMOTE_USER@$REMOTE_HOST:$APP_DIR && \
 ssh -o "StrictHostKeyChecking=no" $REMOTE_USER@$REMOTE_HOST 'bash -s' < sh initService.sh
