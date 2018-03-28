bc9baeaf20d444b23b9ea82a66ee204f44db2497
Raul Muñoz <rmunoz@erpcya.com>
2018-03-28 15:04:25 -0430

    Disable host key checking

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-scp install/build/Adempiere_390LTS.tar.gz $REMOTE_USER@$REMOTE_HOST:$APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < sh initService.sh
+scp -o "StrictHostKeyChecking=no" install/build/Adempiere_390LTS.tar.gz $REMOTE_USER@$REMOTE_HOST:$APP_DIR && \
+ssh -o "StrictHostKeyChecking=no" $REMOTE_USER@$REMOTE_HOST 'bash -s' < sh initService.sh
