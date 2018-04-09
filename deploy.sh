49afc3955c5171b4c5fdccd6dd85d362c0e7fba8
Raul Muñoz <rmunoz@erpcya.com>
2018-04-09 16:34:56 -0430

    change file for copy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-scp -o "StrictHostKeyChecking=no" install/build/Adempiere_390LTS.tar.gz.MD5 $REMOTE_USER@$REMOTE_HOST:$APP_DIR 
-#&& 
-#ssh -o "StrictHostKeyChecking=no" $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./initService.sh
+scp -o "StrictHostKeyChecking=no" /home/travis/build/erpcya/adempiere/install/build/Adempiere_390LTS.tar.gz $REMOTE_USER@$REMOTE_HOST:$APP_DIR 
