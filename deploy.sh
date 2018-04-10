b3be08c2487d6baf153738bfa9803f1c216e7a16
Raul Muñoz <rMunoz@erpcya.com>
2018-04-10 15:28:18 -0430

    Add repository git

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+cd /home/travis/build/erpcya/adempiere/install/build/
+  git init
+  git remote add deploy $REMOTE_USER@$REMOTE_HOST:$APP_DIR 
+  git config user.name $DEPLOY_USER
+  git config user.email $DEPLOY_EMAIL
+  git add Adempiere_390LTS.tar.gz
+  git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
 
-scp -o "StrictHostKeyChecking=no" /home/travis/build/erpcya/adempiere/install/build/Adempiere_390LTS.tar.gz $REMOTE_USER@$REMOTE_HOST:$APP_DIR 
+  echo "Sends build"
+  git push -f deploy master
+
+#scp -o "StrictHostKeyChecking=no" /home/travis/build/erpcya/adempiere/install/build/Adempiere_390LTS.tar.gz 
