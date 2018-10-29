0708263a90df865556dd2462f9a9d0c64afcddb7
Yamel Senih (http://erpya.com/) <ysenih@erpya.com>
2018-10-28 20:30:51 -0500

    Change log level

--- base/src/org/adempiere/process/MigrationLoader.java
+++ base/src/org/adempiere/process/MigrationLoader.java
@@ -46,7 +46,7 @@ public class MigrationLoader {
 		}
 		
 		Adempiere.startupEnvironment(false);
-		CLogMgt.setLevel(Level.CONFIG);
+		CLogMgt.setLevel(Level.SEVERE);
 		
 		if (! DB.isConnected()) {
 			log.info("No DB Connection");
