52ee8b4fc0ca611f8874dc52cd8aad1f607d5367
Raul Muñoz <rMunoz@erpcya.com>
2019-02-18 14:37:19 -0430

    Add support for Generate Order From Task

--- org.adempiere.project/src/main/java/org/compiere/process/ProjectPhaseGenOrder.java
+++ org.adempiere.project/src/main/java/org/compiere/process/ProjectPhaseGenOrder.java
@@ -17,6 +17,10 @@
 package org.compiere.process;
 
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
@@ -29,6 +33,7 @@ import org.compiere.model.MProject;
 import org.compiere.model.MProjectLine;
 import org.compiere.model.MProjectPhase;
 import org.compiere.model.MProjectTask;
+import org.compiere.model.PO;
 import org.compiere.util.Env;
 
 
@@ -56,15 +61,48 @@ public class ProjectPhaseGenOrder  extends ProjectPhaseGenOrderAbstract
 	protected String doIt() throws Exception
 	{
 		int documentTypeTargetId = getParameterAsInt(I_C_Order.COLUMNNAME_C_DocTypeTarget_ID);
-		
+		int projectId = 0;
+		MProjectPhase phase;
+		int phaseId = 0; 
+		List<MProjectLine> projectLines = null;
+		List<MProjectTask> tasks = null;
+		HashMap<String, Object> values = new HashMap<String, Object>();
 		log.info("doIt - C_ProjectPhase_ID=" + getRecord_ID());
 		if (getRecord_ID() == 0)
 			throw new IllegalArgumentException("C_ProjectPhase_ID == 0");
 		if (getDocSubTypeSO()==null)
 			throw new AdempiereException("@NotFound@ @DocSubTypeSO@");
+		PO fromPhase; 
+		if(MProjectTask.Table_Name == getTableName()) {
+			fromPhase = new MProjectTask (getCtx(), getRecord_ID(), get_TrxName());
+			phaseId = ((MProjectTask)fromPhase).getC_ProjectPhase_ID();
+			phase = new MProjectPhase(getCtx(), phaseId, get_TrxName());
+			projectId = phase.getC_Project_ID();
+			values.put("Name", ((MProjectTask)fromPhase).getName());
+			values.put("M_Product_ID", ((MProjectTask)fromPhase).getM_Product_ID());
+			values.put("SeqNo", ((MProjectTask)fromPhase).getSeqNo());
+			values.put("Description", ((MProjectTask)fromPhase).getDescription());
+			values.put("Qty", ((MProjectTask)fromPhase).getQty());
+			values.put("C_ProjectPhase_ID", ((MProjectTask)fromPhase).getC_ProjectPhase_ID());
+			values.put("PriceActual", 0);			
+			projectLines =Arrays.asList(((MProjectTask)fromPhase).getLines());
+		}
+		else if(MProjectPhase.Table_Name  == getTableName()) {
+			fromPhase = new MProjectPhase (getCtx(), getRecord_ID(), get_TrxName());
+			projectId = ((MProjectPhase)fromPhase).getC_Project_ID();
+			values.put("Name", ((MProjectPhase)fromPhase).getName());
+			values.put("M_Product_ID", ((MProjectPhase)fromPhase).getM_Product_ID());
+			values.put("SeqNo", ((MProjectPhase)fromPhase).getSeqNo());
+			values.put("Description", ((MProjectPhase)fromPhase).getDescription());
+			values.put("Qty", ((MProjectPhase)fromPhase).getQty());
+			values.put("C_ProjectPhase_ID", ((MProjectPhase)fromPhase).getC_ProjectPhase_ID());
+			values.put("PriceActual", 0);
+			projectLines =((MProjectPhase)fromPhase).getLines();
+			tasks = ((MProjectPhase)fromPhase).getTasks();
+		}
 		
-		MProjectPhase fromPhase = new MProjectPhase (getCtx(), getRecord_ID(), get_TrxName());
-		MProject fromProject = ProjectGenOrder.getProject (getCtx(), fromPhase.getC_Project_ID(), get_TrxName());
+		
+		MProject fromProject = ProjectGenOrder.getProject (getCtx(), projectId, get_TrxName());
 		if (fromProject.getC_PaymentTerm_ID() <= 0)
 			throw new AdempiereException("@C_PaymentTerm_ID@ @NotFound@");
 
@@ -73,25 +111,26 @@ public class ProjectPhaseGenOrder  extends ProjectPhaseGenOrderAbstract
 		if(documentTypeTargetId > 0) {
 			order.setC_DocTypeTarget_ID(documentTypeTargetId);
 		}
-		order.setDescription(order.getDescription() + " - " + fromPhase.getName());
+		order.setDescription(order.getDescription() + " - " + values.get("Name"));
 		order.saveEx();
-
+		
 		//	Create an order on Phase Level
-		if (fromPhase.getM_Product_ID() != 0) {
+		if ((int)values.get("M_Product_ID") != 0) {
 			MOrderLine orderLine = new MOrderLine(order);
-			orderLine.setLine(fromPhase.getSeqNo());
-			StringBuilder stringBuilder = new StringBuilder(fromPhase.getName());
-			if (fromPhase.getDescription() != null && fromPhase.getDescription().length() > 0)
-				stringBuilder.append(" - ").append(fromPhase.getDescription());
+			orderLine.setLine((int)values.get("SeqNo"));
+			StringBuilder stringBuilder = new StringBuilder(values.get("Name").toString());
+			if (values.get("Description") != null && values.get("Description").toString().length() > 0)
+				stringBuilder.append(" - ").append(values.get("Description").toString());
 			orderLine.setDescription(stringBuilder.toString());
 			//
-			orderLine.setM_Product_ID(fromPhase.getM_Product_ID(), true);
-			orderLine.setQty(fromPhase.getQty());
+			orderLine.setM_Product_ID((int)values.get("M_Product_ID"), true);
+			orderLine.setQty(new BigDecimal(values.get("Qty").toString()));
 			orderLine.setC_Project_ID(fromProject.getC_Project_ID());
-			orderLine.setC_ProjectPhase_ID(fromPhase.getC_ProjectPhase_ID());
+			orderLine.setC_ProjectPhase_ID((int)values.get("C_ProjectPhase_ID"));
 			orderLine.setPrice();
-			if (fromPhase.getPriceActual() != null && fromPhase.getPriceActual().compareTo(Env.ZERO) != 0)
-				orderLine.setPrice(fromPhase.getPriceActual());
+			BigDecimal price = new BigDecimal(values.get("PriceActual").toString());
+			if (price!= null && price.compareTo(Env.ZERO) != 0)
+				orderLine.setPrice(price);
 			orderLine.setTax();
 			if (!orderLine.save())
 				log.log(Level.SEVERE, "doIt - Lines not generated");
@@ -100,7 +139,7 @@ public class ProjectPhaseGenOrder  extends ProjectPhaseGenOrderAbstract
 
 		//	Project Phase Lines
 		AtomicInteger count = new AtomicInteger(0);
-		List<MProjectLine> projectLines = fromPhase.getLines();
+		if(projectLines != null)
 		projectLines.stream()
 				.forEach(projectLine -> {
 					MOrderLine orderLine = new MOrderLine(order);
@@ -123,29 +162,29 @@ public class ProjectPhaseGenOrder  extends ProjectPhaseGenOrderAbstract
 			log.log(Level.SEVERE, "Lines difference - ProjectLines=" + projectLines.size() + " <> Saved=" + count.get());
 
 		//	Project Tasks
-		List<MProjectTask> tasks = fromPhase.getTasks();
-		tasks.stream().filter(task -> task.getM_Product_ID() != 0).forEach(fromTask -> {
-			{
-				MOrderLine orderLine = new MOrderLine(order);
-				orderLine.setLine(fromTask.getSeqNo());
-				StringBuilder stringBuilder = new StringBuilder(fromTask.getName());
-				if (fromTask.getDescription() != null && fromTask.getDescription().length() > 0)
-					stringBuilder.append(" - ").append(fromTask.getDescription());
-				orderLine.setDescription(stringBuilder.toString());
-				orderLine.setM_Product_ID(fromTask.getM_Product_ID(), true);
-				orderLine.setQty(fromTask.getQty());
-				orderLine.setPrice();
-				orderLine.setC_Project_ID(fromProject.getC_Project_ID());
-				orderLine.setC_ProjectPhase_ID(fromTask.getC_ProjectPhase_ID());
-				orderLine.setC_ProjectTask_ID(fromTask.getC_ProjectTask_ID());
-				orderLine.setTax();
-				orderLine.saveEx();
-				count.getAndUpdate(no -> no + 1);
-			}
+		if(tasks != null) {
+			tasks.stream().filter(task -> task.getM_Product_ID() != 0).forEach(fromTask -> {
+				{
+					MOrderLine orderLine = new MOrderLine(order);
+					orderLine.setLine(fromTask.getSeqNo());
+					StringBuilder stringBuilder = new StringBuilder(fromTask.getName());
+					if (fromTask.getDescription() != null && fromTask.getDescription().length() > 0)
+						stringBuilder.append(" - ").append(fromTask.getDescription());
+					orderLine.setDescription(stringBuilder.toString());
+					orderLine.setM_Product_ID(fromTask.getM_Product_ID(), true);
+					orderLine.setQty(fromTask.getQty());
+					orderLine.setPrice();
+					orderLine.setC_Project_ID(fromProject.getC_Project_ID());
+					orderLine.setC_ProjectPhase_ID(fromTask.getC_ProjectPhase_ID());
+					orderLine.setC_ProjectTask_ID(fromTask.getC_ProjectTask_ID());
+					orderLine.setTax();
+					orderLine.saveEx();
+					count.getAndUpdate(no -> no + 1);
+				}
 				});    //	for all lines
-		if (tasks.size() != count.get() - projectLines.size())
-			log.log(Level.SEVERE, "doIt - Lines difference - ProjectTasks=" + tasks.size() + " <> Saved=" + count.get());
-
+			if (tasks.size() != count.get() - projectLines.size())
+				log.log(Level.SEVERE, "doIt - Lines difference - ProjectTasks=" + tasks.size() + " <> Saved=" + count.get());
+		}
 		return "@C_Order_ID@ " + order.getDocumentNo() + " (" + count + ")";
 	}	//	doIt
 
