905e281716d665d33107382fa92203d6495650e4
Yamel Senih <ysenih@erpya.com>
2019-09-06 19:24:29 -0400

    add trx to rule

--- base/src/org/compiere/model/MRule.java
+++ base/src/org/compiere/model/MRule.java
@@ -74,8 +74,10 @@ public class MRule extends X_AD_Rule
 	{
 		Integer key = new Integer (AD_Rule_ID);
 		MRule retValue = (MRule) s_cache.get (key);
-		if (retValue != null)
+		if (retValue != null) {
+			retValue.set_TrxName(trxName);
 			return retValue;
+		}
 		retValue = new MRule (ctx, AD_Rule_ID, trxName);
 		if (retValue.get_ID () != 0)
 			s_cache.put (key, retValue);
