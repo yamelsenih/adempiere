9ddbfa9c9a6f959c22bc1bafab8f8a21371ddcba
Yamel Senih <ysenih@erpya.com>
2019-09-19 04:31:36 -0400

    Add commit

--- base/src/org/compiere/model/X_AD_OrgInfo.java
+++ base/src/org/compiere/model/X_AD_OrgInfo.java
@@ -29,7 +29,7 @@ public class X_AD_OrgInfo extends PO implements I_AD_OrgInfo, I_Persistent
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = 20190501L;
+	private static final long serialVersionUID = 20190919L;
 
     /** Standard Constructor */
     public X_AD_OrgInfo (Properties ctx, int AD_OrgInfo_ID, String trxName)
